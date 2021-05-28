3a00e80d49bc32435192edace20c08b8a501ef7d
WonjunJang <wonjun84@naver.com>
2021-05-28 22:54:19 +0900

    setting: Set style, tsconfig

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,8 @@
 import './App.scss';
-import { Button } from '../components';
 
 function App() {
   return (
     <div className="App">
-      <Button type={'button'} value={'버튼'} style={null}/>
     </div>
   );
 }
