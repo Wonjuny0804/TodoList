db380c52a9ac37142c4534e0464d585394f3f89b
wonjun <wonwonjun@gmail.com>
2021-06-30 10:12:41 +0900

    Resolve eslint error issue, tsconfig parsing error

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.scss';
 import {
-  Signature
-} from '../components'
+  Main
+} from '../pages'
 
 function App() {
   return (<div className="App">
-    <Signature first_name="Wonjun" last_name="Jang" occupation="Frontend Engineer" description={['Creative Tech Enthusiast', 'interested in Art']} />
+    <Main />
   </div>
   );
 }
