98f316ffe54d24eba9469832ed6d876b49ccb47c
wonjun <wonwonjun@gmail.com>
2021-06-30 10:12:12 +0900

    Create Main App page

---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+
+    return (
+        <>
+            MainPage
+        </>
+    )
+}
+
+export default Main
---
+++ src/pages/index.tsx
@@ -0,0 +1,2 @@
+export { default as Main } from './Main'
+export { default as Main2 } from './Main'
