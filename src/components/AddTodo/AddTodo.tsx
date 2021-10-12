8a922969f6cdf1c3dcf7f305c05e267a1615df67
wonjun <wonwonjun@gmail.com>
2021-10-12 09:57:16 +0900

    Add reseting value feature

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, useState } from 'react';
+import React, { MouseEvent, useState, useRef, useCallback } from 'react';
 import { Button } from '../index';
 import {ReactComponent as Add} from '../Button/assets/Add.svg'; 
 import styles from './AddTodo.module.scss';
@@ -14,8 +14,9 @@ interface AddTodoProps {
 function AddTodo({ className, addTodo, currentTodos }: AddTodoProps):JSX.Element {
 
     const [writtenTodo, setWrittenTodo] = useState<string>("");
+    const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleAddTodoClick = () => {
+    const handleAddTodoClick = useCallback(() => {
         if (writtenTodo === "") return;
 
         const currentTodosID = currentTodos.map(todo => todo.id);
@@ -26,14 +27,35 @@ function AddTodo({ className, addTodo, currentTodos }: AddTodoProps):JSX.Element
             addTodo([...currentTodos, { id: maxTodoID + 1, todo: writtenTodo, done: false}]);
         }
 
-    }
+        if (inputRef.current) {
+            inputRef.current.value = "";
+            setWrittenTodo("");
+        }
+    }, [currentTodos, addTodo, writtenTodo]);
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         const target = e.target as HTMLInputElement;
         const value = target.value;
 
         setWrittenTodo(value);
-    }
+    },[setWrittenTodo]);
+
+    const handlePressEnter = useCallback((e: React.KeyboardEvent) => {
+        if (e.key !== "Enter") return;
+
+        const currentTodosID = currentTodos.map(todo => todo.id);
+        if (currentTodosID.length === 0) {
+            addTodo([{ id: 1, todo: writtenTodo, done: false }]);
+        } else {
+            const maxTodoID = Math.max(...currentTodosID);
+            addTodo([...currentTodos, { id: maxTodoID + 1, todo: writtenTodo, done: false}]);
+        }
+        
+        if (inputRef.current) {
+            inputRef.current.value = "";
+            setWrittenTodo("");
+        }
+    }, [setWrittenTodo, addTodo, currentTodos, writtenTodo]);
     
     return (
         <section className={className ? classNames(styles.addTodoWrapper, className) : styles.addTodoWrapper}>
@@ -42,6 +64,8 @@ function AddTodo({ className, addTodo, currentTodos }: AddTodoProps):JSX.Element
                 type="text" 
                 className={styles.addTodoInput}
                 onChange={handleInputChange}
+                onKeyUp={handlePressEnter}
+                ref={inputRef}
             />
             <Button
                 type="button"
