01b68a75ce2e18010edf6bf789505a605cb77e92
wonjun <wonwonjun@gmail.com>
2021-10-16 08:58:48 +0900

    Add delete Item svgs and Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,14 +1,18 @@
-import { ReactComponent as Logo} from "../../assets/Logo.svg";
-import { ReactComponent as Checked} from "../../assets/checked.svg";
+import { ReactComponent as Logo } from "../../assets/Logo.svg";
+import { ReactComponent as Checked } from "../../assets/checked.svg";
+import { ReactComponent as Delete } from "../../assets/delete.svg";
+import { ReactComponent as DeleteCompleted } from "../../assets/deleteCompleted.svg";
 
 interface IconProps {
-  name: "logo" | "checked";
+  name: "logo" | "checked" | "delete" | "deleteCompleted";
 }
 
 const Icon = ({ name }: IconProps): JSX.Element => {
   const iconLists = {
     logo: <Logo />,
-    checked: <Checked />
+    checked: <Checked />,
+    delete: <Delete />,
+    deleteCompleted: <DeleteCompleted />
   }
 
   const returnValue = iconLists[name];
